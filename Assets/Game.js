ce4f997e2aa0d4d3b6919b9ad5b5f16d387d9609
Eivin Landa <landa@drivdigital.no>
2016-09-10 20:58:55 +0200

    Updated some stuff

--- Assets/Game.js
+++ Assets/Game.js
@@ -1,10 +1,11 @@
 ﻿#pragma strict
 var offset :Vector3;
 var scale :Vector3 = new Vector3(128,128,1);
-var player_1_piece :RectTransform;
-var player_2_piece :RectTransform;
+var playing_piece :RectTransform;
 private var p1_pieces :Array;
 private var p2_pieces :Array;
+
+var p2_image :Sprite;
 
 private var player_playing :int = 0;
 function v23( x :float, y:float ) {
@@ -18,20 +19,34 @@ function Start () {
 	// Create 4 pieces for each player
 	for ( var i = 0; i < 4; i ++ ) {
 		// Player 1 piece
-		piece = Instantiate( player_1_piece, Vector3(), Quaternion.identity );
+		piece = Instantiate( playing_piece, Vector3(), Quaternion.identity );
 		piece.SetParent( transform );
 		p1_pieces.push( piece );
 		piece.localPosition = v23( i, i );
 		piece.localScale = Vector3(1,1,1);
 		// Player 2 piece
-		piece = Instantiate( player_2_piece, Vector3(), Quaternion.identity );
+		piece = Instantiate( playing_piece, Vector3(), Quaternion.identity );
 		piece.SetParent( transform );
 		p2_pieces.push( piece );
 		piece.localPosition = v23( i, 3-i );
 		piece.localScale = Vector3(1,1,1);
+
+		// Get all components of type Image that are children of this GameObject.
+		var images = piece.GetComponentsInChildren.<UnityEngine.UI.Image>();
+		// Loop through each image and set it's Sprite to the other Sprite.
+		for ( var image in images ) {
+			image.sprite = p2_image;
+		}
 	}
 }
 
 function Update () {
 
+}
+
+function MouseClicked( e: UnityEngine.EventSystems.BaseEventData ) {
+	Debug.Log( e.selectedObject );
+	Debug.Log( e );
+	// e.selectedObject.transform.position.x = e.selectedObject.transform.position.x + 20;
+	Debug.Log('test');
 }
---
+++ Assets/PlayerPiece.js
@@ -0,0 +1,5 @@
+﻿#pragma strict
+
+function Test(e) {
+	transform.position.x = transform.position.x + 10;
+}
