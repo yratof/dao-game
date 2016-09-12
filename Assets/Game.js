5100bc406ef4c7f2f4f3ce54386d205cb1ba359d
Eivin Landa <landa@drivdigital.no>
2016-09-12 09:18:54 +0200

    Added selecting and predicting

--- Assets/Game.js
+++ Assets/Game.js
@@ -1,9 +1,14 @@
 ﻿#pragma strict
 var offset :Vector3;
 var scale :Vector3 = new Vector3( 175, 175, 1);
-var playing_piece :RectTransform;
+var playing_piece :Transform;
 private var p1_pieces :Array;
 private var p2_pieces :Array;
+
+private var team_turn :int = 1;
+
+var selector:Transform;
+var predictor:Transform;
 
 var p2_image :Sprite;
 
@@ -14,8 +19,10 @@ function v23( x :float, y:float ) {
 }
 function Start () {
 	var piece :Transform;
+	var pp : PlayerPiece;
 	p1_pieces = new Array();
 	p2_pieces = new Array();
+
 	// Create 4 pieces for each player
 	for ( var i = 0; i < 4; i ++ ) {
 		// Player 1 piece
@@ -24,12 +31,23 @@ function Start () {
 		p1_pieces.push( piece );
 		piece.localPosition = v23( i, i );
 		piece.localScale = Vector3(1,1,1);
+		pp = piece.gameObject.GetComponent( PlayerPiece );
+		pp.selector =  selector;
+		pp.predictor = predictor;
+		pp.team = 1;
+		pp.game = this;
+
 		// Player 2 piece
 		piece = Instantiate( playing_piece, Vector3(), Quaternion.identity );
 		piece.SetParent( transform );
 		p2_pieces.push( piece );
 		piece.localPosition = v23( i, 3-i );
 		piece.localScale = Vector3(1,1,1);
+		pp = piece.gameObject.GetComponent( PlayerPiece );
+		pp.selector =  selector;
+		pp.predictor = predictor;
+		pp.team = 2;
+		pp.game = this;
 
 		// Get all components of type Image that are children of this GameObject.
 		var images = piece.GetComponentsInChildren.<UnityEngine.UI.Image>();
@@ -44,9 +62,6 @@ function Update () {
 
 }
 
-function MouseClicked( e: UnityEngine.EventSystems.BaseEventData ) {
-	Debug.Log( e.selectedObject );
-	Debug.Log( e );
-	// e.selectedObject.transform.position.x = e.selectedObject.transform.position.x + 20;
-	Debug.Log('test');
+function canMove( piece_team : int ) {
+	return team_turn == piece_team;
 }
--- Assets/PlayerPiece.js
+++ Assets/PlayerPiece.js
@@ -1,5 +1,26 @@
 ﻿#pragma strict
-
-function Test(e) {
-	transform.localPosition.x = transform.localPosition.x + 175;
+var selector:Transform;
+var predictor:Transform;
+var team:int;
+var game: Game;
+function Select(e) {
+	if ( game.canMove( team ) ) {
+		selector.localPosition = transform.localPosition;
+	}
 }
+function Predict(e:UnityEngine.EventSystems.BaseEventData) {
+	var pointer : UnityEngine.EventSystems.PointerEventData = e as UnityEngine.EventSystems.PointerEventData;
+	var temp_pos = Vector2();
+
+	// Debug.Log( pointer.position );
+	predictor.position = pointer.position;
+
+	temp_pos.x = pointer.position.x - transform.position.x;
+	temp_pos.y = pointer.position.y - transform.position.y;
+	temp_pos.Normalize();
+	Debug.Log( '---' );
+	Debug.Log( temp_pos );
+
+}
+
+
